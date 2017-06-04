cb7db5a94e5300ab6f21c3e0f70bdd97797c355b
daliborpavicic <dalibor-pavicic@hotmail.com>
2017-06-04 15:48:15 +0200

    Add seed files for ng2 app

---
+++ examples/angular2/app/company-registry-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'company-registry',
+    template: `<h2>Welcome to Company registry!</h2>`
+})
+export class CompanyRegistryAppComponent {
+
+}
