06b1276c0c5861c8512c14c915d15d091532571e
daliborpavicic <dalibor-pavicic@hotmail.com>
2017-08-06 14:56:19 +0200

    [ng2] Organize exports

--- examples/angular2/src/app/place/index.ts
+++ examples/angular2/src/app/place/index.ts
@@ -2,3 +2,4 @@ export * from './place.component';
 export * from './place-list.component';
 export * from './place-list-resolver.service';
 export * from './shared/index';
+export * from './place-resolver.service';
