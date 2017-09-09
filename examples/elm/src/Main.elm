04a0cef8a09719f89c8bf59db93df00ef499905a
daliborpavicic <dalibor-pavicic@hotmail.com>
2017-09-09 16:43:37 +0200

    Fetch place from MongoLab, edit it and submit again

--- examples/elm/src/Main.elm
+++ examples/elm/src/Main.elm
@@ -1,8 +1,11 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, button, input, table, thead, tbody, tr, th, td)
+import Html exposing (Html, text, div, button, input, table, thead, tbody, tr, th, td, br, p)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import Json.Decode as Decode
+import Json.Encode as Encode
+import Http
 
 type alias Place =
   { id:  String
@@ -18,20 +21,72 @@ setName : Place -> String -> Place
 setName place placeName =
     { place | name = placeName }
 
+placeDecoder : Decode.Decoder Place
+placeDecoder =
+  Decode.map3 Place
+    (Decode.field "_id" Decode.string)
+    (Decode.field "postalCode" Decode.string)
+    (Decode.field "name" Decode.string)
+
+placeEncoder : Place -> Encode.Value
+placeEncoder place =
+  Encode.object
+    [ ("_id", Encode.string place.id )
+    , ("postalCode", Encode.string place.postalCode)
+    , ("name", Encode.string place.name)
+    ]
+
+collectionUrl =
+  "https://api.mongolab.com/api/1/databases/angulardb/collections/places"
+
+placesUrl =
+  withApiKey collectionUrl
+
+placeUrl placeId =
+  withApiKey <| String.join "/" [ collectionUrl, placeId ]
+
+withApiKey url =
+  url ++ "?apiKey=ibGX1jtg9reXu0NcrrLwLuv5jpiUULnw"
+
+fetchPlaceById placeId =
+  let
+    url = placeUrl placeId
+    request =
+      Http.get url placeDecoder
+  in
+    Http.send NewPlace request
+
+submitPlace place =
+  let
+    url = placesUrl
+    placeToSubmit = { place | id = place.postalCode }
+    jsonBody = Http.stringBody "application/json" (Encode.encode 0 (placeEncoder placeToSubmit))
+    request =
+      Http.post url jsonBody Decode.string
+  in
+    Http.send SubmitPlace request
+
+
 type alias Model =
   { editedPlace : Place
   , places : List Place
+  , placeId: String
+  , errorMessage: String
   }
 
 type Msg
     = SetPostalCode String
     | SetName String
     | SavePlace Place
+    | FetchPlace String
+    | SetPlaceId String
+    | NewPlace (Result Http.Error Place)
+    | SubmitPlace (Result Http.Error String)
 
 init : ( Model, Cmd Msg )
 init =
   let
-    initialModel = { editedPlace = Place "" "" "", places = [] }
+    initialModel = { editedPlace = Place "" "" "", places = [], placeId = "", errorMessage = "" }
   in
     ( initialModel, Cmd.none )
 
@@ -51,15 +106,48 @@ update msg model =
         ( { model | editedPlace = place }, Cmd.none )
 
     SavePlace newPlace ->
-      ( { model | places = newPlace :: model.places }, Cmd.none )
+      ( { model | places = newPlace :: model.places }, submitPlace newPlace )
+
+    SetPlaceId placeIdToFetch ->
+      ( { model | placeId = placeIdToFetch }, Cmd.none )
+
+    FetchPlace placeIdToFetch ->
+      ( model, fetchPlaceById placeIdToFetch )
+
+    NewPlace (Ok newPlace) ->
+      ( { model | editedPlace = newPlace, errorMessage = "" }, Cmd.none )
+
+    NewPlace (Err err) ->
+      case err of
+        Http.BadStatus response ->
+          ( { model | errorMessage = (toString response.status.code) ++ " " ++  response.status.message }, Cmd.none )
+
+        _ ->
+          ( model, Cmd.none )
+
+    SubmitPlace (Ok message) ->
+      ( model, Cmd.none )
+
+    SubmitPlace (Err _) ->
+      ( model, Cmd.none )
 
 view : Model -> Html Msg
 view model =
   div []
-    [ input [ type_ "text", placeholder "Postal Code", onInput SetPostalCode ] []
-    , input [ type_ "text", placeholder "Name", onInput SetName ] []
+    [ fetchPlaceForm model
+    , input [ type_ "text", placeholder "Postal Code", defaultValue model.editedPlace.postalCode, onInput SetPostalCode ] []
+    , input [ type_ "text", placeholder "Name", defaultValue model.editedPlace.name, onInput SetName ] []
     , button [ onClick (SavePlace model.editedPlace) ] [ text "Save" ]
     , placesList model.places
+    ]
+
+fetchPlaceForm : Model -> Html Msg
+fetchPlaceForm model =
+  div []
+    [ input [ type_ "text", placeholder "Enter postal code...", onInput SetPlaceId ] []
+    , button [ onClick (FetchPlace model.placeId) ] [ text "Get place" ]
+    , p [ style [ ("color", "red") ] ] [ text model.errorMessage ]
+    , br [] []
     ]
 
 placesList places =
