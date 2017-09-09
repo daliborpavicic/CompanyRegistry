b24cf6cbad743971ee64dc0ef6467e2d70270c61
daliborpavicic <dalibor-pavicic@hotmail.com>
2017-09-09 21:42:22 +0200

    Add bootstrap style on form

--- examples/elm/src/Main.elm
+++ examples/elm/src/Main.elm
@@ -1,6 +1,6 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, button, input, table, thead, tbody, tr, th, td, br, p)
+import Html exposing (Html, text, div, button, input, table, thead, tbody, tr, th, td, br, p, form, label, i, h3, hr)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import Json.Decode as Decode
@@ -173,14 +173,48 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  Debug.log (toString model)
   div []
     [ fetchPlaceForm model
-    , input [ type_ "text", placeholder "Postal Code", value model.editedPlace.postalCode, onInput SetPostalCode ] []
-    , input [ type_ "text", placeholder "Name", value model.editedPlace.name, onInput SetName ] []
-    , button [ onClick (SavePlace model.editedPlace) ] [ text "Save" ]
-    , button [ onClick (DeletePlace model.editedPlace.postalCode) ] [ text "Delete" ]
-    , button [ onClick RevertChanges ] [ text "Revert" ]
+    , h3 [] [ text "Edit place" ]
+    , hr [] []
+    , Html.form [ class "form-horizontal" ]
+      [ div [ class "form-group" ]
+        [ label [ class "control-label col-sm-2" ] [ text "Postal Code" ]
+        , div [ class "col-sm-6 col-md-4" ]
+            [ input
+              [ type_ "text"
+              , class "form-control"
+              , placeholder "Postal Code"
+              , value model.editedPlace.postalCode
+              , onInput SetPostalCode
+              ] []
+            ]
+        ]
+      , div [ class "form-group" ]
+        [ label [ class "control-label col-sm-2" ] [ text "Name" ]
+        , div [ class "col-sm-6 col-md-4" ]
+            [ input
+              [ type_ "text"
+              , class "form-control"
+              , placeholder "Name"
+              , value model.editedPlace.name
+              , onInput SetName
+              ] []
+            ]
+        ]
+       , div [ class "col-sm-offset-2" ]
+          [ button [ class "btn btn-primary", type_ "submit", onClick (SavePlace model.editedPlace) ] [ text "Save" ]
+          , button [ class "btn btn-warning", type_ "button", onClick RevertChanges ] [ text "Revert" ]
+          , button [ class "btn btn-danger", type_ "button", onClick (DeletePlace model.editedPlace.postalCode) ] [ text "Delete" ]
+          , button
+              [ class "btn btn-default"
+              , type_ "button"
+              ]
+              [ i [ class "fa fa-chevron-left" ] []
+              , text " Back"
+              ]
+          ]
+      ]
     , placesList model.places
     ]
 
