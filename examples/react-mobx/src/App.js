bcf073ad0363fd096526c5562f317270b704185a
daliborpavicic <dalibor-pavicic@hotmail.com>
2017-11-26 15:31:18 +0100

    [react-mobx] Add new dpendencies (mobx, mobx-react, react-router, prop-types)

--- examples/react-mobx/src/App.js
+++ examples/react-mobx/src/App.js
@@ -1,34 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { PlaceService } from './api/crudServices';
 
 class App extends Component {
-  componentDidMount() {
-    const logResource = resource => {
-      console.log(resource);
-    };
-    const logError = err => {
-      console.log(err);
-    };
-
-    const testId = '_test_';
-    const newPlace = { _id: testId, name: '_InitialName_', postalCode: '_test_' };
-    const updatedPlace = Object.assign({}, newPlace, { name: '_NewName_' });
-
-    const promises = [
-      PlaceService.saveOrUpdateResource(updatedPlace),
-      PlaceService.getAllResources(),
-      PlaceService.getResourceById(testId),
-      PlaceService.saveOrUpdateResource(newPlace),
-      PlaceService.deleteResourceWithId(testId),
-    ];
-
-    Promise.all(promises).then(
-      results => results.forEach(result => logResource(result)),
-      err => logError(err)
-    );
-  }
-
   render() {
     return (
       <div className="App">
